5d43313b1d4a18e7e85aa240511c91df6bcc1d06
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-06 11:43:57 -0500

    Routes, rutas hijas y lazyload

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
-
-
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { RegistroComponent } from './pages/registro/registro.component';
     RegistroComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }

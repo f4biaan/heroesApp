b947e3ae02cb818db6a66a59d9390bdde612d315
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-06 17:58:36 -0500

    MAquetado iniciaÃl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
-import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +16,6 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -4,8 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: [
-  ]
+    `
+      .container {
+        margin: 20px;
+      }
+    `,
+  ],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}

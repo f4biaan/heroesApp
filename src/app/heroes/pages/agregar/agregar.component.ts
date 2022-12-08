26db466b3d1aee5cfa110982c3584a9df1f9dbcf
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-07 19:04:57 -0500

    Agregar heroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent {
+  publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
+  ];
 
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  };
+
+  constructor(private heroesService: HeroesService) {}
+
+  agregar() {
+    if (this.heroe.superhero.trim().length === 0) {
+      return;
+    }
+
+    this.heroesService
+      .agregarHeroe(this.heroe)
+      .subscribe((heroe) => console.log(heroe));
+  }
 }

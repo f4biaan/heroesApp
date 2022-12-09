f410fa4896785b77ee17744a05fdada570de0e37
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-09 00:33:13 -0500

    pipe pure

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -3,9 +3,11 @@ import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
   name: 'imagen',
+  pure: true,
 })
 export class ImagenPipe implements PipeTransform {
   transform(heroe: Heroe): string {
+    console.log('imagen se proceso...');
     if (!heroe.id && !heroe.alt_img) {
       return 'assets/no-image.png';
     } else if (heroe.alt_img) {

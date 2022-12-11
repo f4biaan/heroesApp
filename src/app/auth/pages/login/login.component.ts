9712262fb845d3651b9ee5980e8cdda5f18c93de
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-10 19:11:36 -0500

    Login auth/service

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginComponent {
-
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   login() {
     // ir al backend
     // un usuario
+    this.authService.login().subscribe((resp) => {
+      console.log(resp);
+      if (resp.id) {
+        this.router.navigate(['/heroes']);
+      }
+    });
 
-    this.router.navigate(['/heroes']);
   }
-
 }
